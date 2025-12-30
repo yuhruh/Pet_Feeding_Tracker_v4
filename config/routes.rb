d08ec13edcd6fd239fee0f7415ca01abb65e11a4
Rita <yuhruhgrape@gmail.com>
2025-12-30 22:43:49 +0800

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   root "pages#hero_section"
   get "/home", to: "pages#hero_section"
   get "/about", to: "pages#about"
