4feeadc5b14301a20c80ead62338b38cf2fb620a
Rita <yuhruhgrape@gmail.com>
2026-01-02 23:08:29 +0800

    differentiate 1st and existing user sign in flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,19 @@ class SessionsController < ApplicationController
 
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
-      flash[:notice] = "You have been signed in"
-      start_new_session_for user
-      redirect_to after_authentication_url
+      user.last_sign_in_at = user.current_sign_in_at
+      user.current_sign_in_at = Time.current
+      user.sign_in_count = user.sign_in_count.to_t + 1
+      user.save(validate: false)
+
+      if user.new_uers?
+        flash[:notice] = "Hello, #{Current.user.username.capitalize} 👋. This is your first time to sign in, please add a new cat first for further tracker."
+        redirect_to after_authentication_url
+      else
+        start_new_session_for user
+        flash[:notice] = "Welcome back to Cat Feeding Tracker, #{Current.user.username.capitalzie}"
+        redirect_to users_path
+      end
     else
       redirect_to new_session_path, alert: "Try another email address or password."
     end
