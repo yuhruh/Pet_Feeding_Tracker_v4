a310cc6cb68316529bfc948cce62375359943f81
Rita <yuhruhgrape@gmail.com>
2026-01-08 17:31:57 +0800

    update new tracker form

--- app/helpers/trackers_helper.rb
+++ app/helpers/trackers_helper.rb
@@ -1,2 +1,11 @@
 module TrackersHelper
+  def set_current_time
+    Time.zone = Current.user.timezone
+    Time.current.strftime("%H:%M")
+  end
+
+  def set_current_date
+    Time.zone = Current.user.timezone
+    Date.current.strftime("%Y-%m-%d")
+  end
 end
