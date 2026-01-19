28d0a30d91693873ff643e974a1765879c748a8e
Rita <yuhruhgrape@gmail.com>
2026-01-19 22:04:09 +0800

    feat: add connectedservice in registrations_controller for google signup function

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,20 +1,50 @@
 class RegistrationsController < ApplicationController
   allow_unauthenticated_access only: %i[new create]
+  before_action :resume_session, only: %i[new create]
 
   def new
     @user = User.new
+
+    if session["omniauth.auth"].present?
+      auth_data = session["omniauth.auth"]
+      auth_info = auth_data["info"] || {}
+    
+      # Handle LINE-specific username structure
+      if auth_data["provider"] == "line"
+        # LINE typically provides displayName or name
+        @user.username = auth_info["name"] || 
+                        auth_info["displayName"] || 
+                        "line_user_#{auth_data['uid']}"
+        
+        # Email might not be available for LINE without permission
+        @user.email_address = auth_info["email"] if auth_info["email"].present?
+      else
+        # Handle other providers
+        @user.username = auth_info["name"] || auth_info["email"]&.split('@')&.first
+        @user.email_address = auth_info["email"]
+      end
+    end
   end
 
   def create
     @user = User.new(user_params)
     @user.sign_in_count = 1
     if @user.save
+      if session["omniauth.auth"].present?
+        auth_data = session["omniauth.auth"]
+        @user.connected_services.create!(
+          provider: auth_data["provider"],
+          uid: auth_data["uid"]
+        )
+        session.delete("omniauth.auth")
+      end
       start_new_session_for @user
       UserMailer.with(user: @user).welcome.deliver_later
       redirect_to new_pet_path, notice: "Hello, #{Current.user.username.capitalize} 👋. You've successfully signed up to Cat Feeding Tracker App and
                                       this is your first time to sign in, please add a new cat first for further tracker.
                                       Welcome #{@user.username.capitalize}!"
     else
+      flash[:alert] = @user.errors.full_messages.join(", ")
       render :new, status: :unprocessable_entity
     end
   end
