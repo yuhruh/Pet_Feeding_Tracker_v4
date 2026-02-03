e8fd3fd94781e3f8240d27236ac2513ee9b1f61a
Rita <yuhruhgrape@gmail.com>
2026-02-03 22:14:03 +0800

    fix: ActionView::Template::Error

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,6 @@ module PetTrackerV4
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.paths << Rails.root.join("app", "assets", "images")
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,6 @@ Rails.application.configure do
 
   # Full error reports are disabled.
   config.consider_all_requests_local = false
-  config.assets.compile = true
 
   # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
