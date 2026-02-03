1c4515a8aa53b26eb57c21bce8535e5c990e5f75
Rita <yuhruhgrape@gmail.com>
2026-02-03 14:52:50 +0800

    modify cache_store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,10 +51,10 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
     # Replace the default in-process memory cache store with a durable alternative.
-    # config.cache_store = :solid_cache_store
-    config.cache_store = :redis_cache_store, {
-    url: ENV["REDIS_URL"]
-  }
+    config.cache_store = :solid_cache_store
+  #   config.cache_store = :redis_cache_store, {
+  #   url: ENV["REDIS_URL"]
+  # }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   # config.active_job.queue_adapter = :solid_queue
