61cf333f6e6e04efb15c3211902e995444e80a2f
Rita <yuhruhgrape@gmail.com>
2026-02-03 20:23:37 +0800

    style: fix rubocop offenses

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,34 +1,34 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development?
 
-  #GOOGLE
+  # GOOGLE
   provider :google_oauth2,
-          Rails.application.credentials.dig(:google, :client_id), 
-          Rails.application.credentials.dig(:google, :client_secret), 
+          Rails.application.credentials.dig(:google, :client_id),
+          Rails.application.credentials.dig(:google, :client_secret),
           { scope: "email, profile",
             callback_path: "/auth/google_oauth2/callback",
             request_path: "/auth/google_oauth2"
           }
 
-  #LINE
+  # LINE
   provider :line,
-          Rails.application.credentials.dig(:line, :client_id), 
-          Rails.application.credentials.dig(:line, :client_secret), 
-          { scope: "profile openid email", 
+          Rails.application.credentials.dig(:line, :client_id),
+          Rails.application.credentials.dig(:line, :client_secret),
+          { scope: "profile openid email",
             callback_path: "/auth/line/callback",
             request_path: "/auth/line"
           }
-  
-  #GITHUB
+
+  # GITHUB
   provider :github,
-          Rails.application.credentials.dig(:github, :client_id), 
-          Rails.application.credentials.dig(:github, :client_secret), 
+          Rails.application.credentials.dig(:github, :client_id),
+          Rails.application.credentials.dig(:github, :client_secret),
           { scope: "user:email",
             callback_path: "/auth/github/callback",
             request_path: "/auth/github"
           }
-          
-          
+
+
 
   # if Rails.env.production?
   # provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
