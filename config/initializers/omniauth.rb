bb59b9c3bdfe211171f7f0a3098286cca690b2bd
Rita <yuhruhgrape@gmail.com>
2026-02-03 20:22:57 +0800

    feat: unify code for oomniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,32 +1,61 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development?
 
-  if Rails.env.production?
-    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-      scope: "email, profile",
-      callback_path: "/auth/google_oauth2/callback",
-      request_path: "/auth/google_oauth2"
-    }
+  #GOOGLE
+  provider :google_oauth2,
+          Rails.application.credentials.dig(:google, :client_id), 
+          Rails.application.credentials.dig(:google, :client_secret), 
+          { scope: "email, profile",
+            callback_path: "/auth/google_oauth2/callback",
+            request_path: "/auth/google_oauth2"
+          }
 
-    provider :line, ENV["LINE_CLIENT_ID"], ENV["LINE_CLIENT_SECRET"], {
-      scope: "profile openid email",
-      callback_path: "/auth/line/callback",
-      request_path: "/auth/line"
-    }
+  #LINE
+  provider :line,
+          Rails.application.credentials.dig(:line, :client_id), 
+          Rails.application.credentials.dig(:line, :client_secret), 
+          { scope: "profile openid email", 
+            callback_path: "/auth/line/callback",
+            request_path: "/auth/line"
+          }
+  
+  #GITHUB
+  provider :github,
+          Rails.application.credentials.dig(:github, :client_id), 
+          Rails.application.credentials.dig(:github, :client_secret), 
+          { scope: "user:email",
+            callback_path: "/auth/github/callback",
+            request_path: "/auth/github"
+          }
+          
+          
 
-    provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], {
-      scope: "user:email",
-      callback_path: "/auth/github/callback",
-      request_path: "/auth/github"
-    }
+  # if Rails.env.production?
+  # provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
+  #   scope: "email, profile",
+  #   callback_path: "/auth/google_oauth2/callback",
+  #   request_path: "/auth/google_oauth2"
+  # }
 
-  elsif Rails.env.development?
-    provider :google_oauth2, Rails.application.credentials.dig(:google, :client_id), Rails.application.credentials.dig(:google, :client_secret), {
-      scope: "email, profile"
-    }
-    provider :line, Rails.application.credentials.dig(:line, :client_id), Rails.application.credentials.dig(:line, :client_secret), scope: "profile openid email"
-    provider :github, Rails.application.credentials.dig(:github, :client_id), Rails.application.credentials.dig(:github, :client_secret), scope: "user:email"
-  end
+  # provider :line, ENV["LINE_CLIENT_ID"], ENV["LINE_CLIENT_SECRET"], {
+  #   scope: "profile openid email",
+  #   callback_path: "/auth/line/callback",
+  #   request_path: "/auth/line"
+  # }
+
+  # provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], {
+  #   scope: "user:email",
+  #   callback_path: "/auth/github/callback",
+  #   request_path: "/auth/github"
+  # }
+
+  # elsif Rails.env.development?
+  #   provider :google_oauth2, Rails.application.credentials.dig(:google, :client_id), Rails.application.credentials.dig(:google, :client_secret), {
+  #     scope: "email, profile"
+  #   }
+  #   provider :line, Rails.application.credentials.dig(:line, :client_id), Rails.application.credentials.dig(:line, :client_secret), scope: "profile openid email"
+  #   provider :github, Rails.application.credentials.dig(:github, :client_id), Rails.application.credentials.dig(:github, :client_secret), scope: "user:email"
+  # end
 
   OmniAuth.config.on_failure = Proc.new do |env|
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure
