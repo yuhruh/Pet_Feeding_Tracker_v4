c3555684cb60f2eb24bb33678aba69deb77222e2
Rita <yuhruhgrape@gmail.com>
2026-02-04 20:01:27 +0800

    fix: render 500 error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,24 +6,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email_address: params[:email_address])
+    @user = User.find_by(email_address: params[:email_address])
 
-    if user
-      if user.authenticate(params[:password])
-        start_new_session_for user
-        user.update_columns(
-          last_sign_in_at: user.current_sign_in_at,
+    if @user
+      if @user.authenticate(params[:password])
+        start_new_session_for @user
+        @user.update_columns(
+          last_sign_in_at: @user.current_sign_in_at,
           current_sign_in_at: Time.current,
-          sign_in_count: user.sign_in_count.to_i + 1
+          sign_in_count: @user.sign_in_count.to_i + 1
         )
 
-        if user.new_user?
+        if @user.new_user?
           redirect_to new_pet_path, notice: "Hello, #{Current.user.username.capitalize} 👋. This is your first time to sign in, please add a new cat first for further tracker."
         else
           redirect_to pets_path, notice: "Welcome back to Cat Feeding Tracker, #{Current.user.username.capitalize}"
         end
       else
-        msg = if user.connected_services.any?
+        msg = if @user.connected_services.any?
                 "You've previously signed in using your #{connected_services_string(user)} account. Please use that to sign in."
         else
                 "The password is not correct"
@@ -31,7 +31,6 @@ class SessionsController < ApplicationController
         redirect_to new_session_path, alert: msg, status: :see_other
       end
     else
-      Rails.logger.info "Unregistered user attempt with email: #{params[:email_address]}. Redirecting to sign up."
       redirect_to new_registrations_path, alert: "This email has not been signed up yet. Please sign up first", status: :see_other
     end
   end
