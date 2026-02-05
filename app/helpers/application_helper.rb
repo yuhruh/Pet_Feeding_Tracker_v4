8cb8aaa69a8fb1d3847454e0e314754ba2695fa9
Rita <yuhruhgrape@gmail.com>
2026-02-05 22:18:06 +0800

    style: fix rubocop offenses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def locale_to_word_flag(locale)
     locales = {
-      en: '🇺🇸 English',
-      "zh-TW": '🇹🇼 繁體中文',
-      "ja": '🇯🇵 日本語'
+      en: "🇺🇸 English",
+      "zh-TW": "🇹🇼 繁體中文",
+      "ja": "🇯🇵 日本語"
     }
 
     locales[locale.to_sym]
