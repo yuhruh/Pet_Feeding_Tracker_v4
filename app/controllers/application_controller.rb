79e3f306842823b27334f3c08b98bbf8eda03efd
Rita <yuhruhgrape@gmail.com>
2026-02-06 21:42:56 +0800

    feat: change edge, firefox and github as svg files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include Authentication
-  include SvgHelper
+  helper SvgHelper
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :set_locale
