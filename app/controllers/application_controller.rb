4f3575eb296ec5e00627adf3dbaeeefabcd57dbf
Rita <yuhruhgrape@gmail.com>
2026-02-06 20:26:26 +0800

    fix: fix system test fail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include Authentication
+  include SvgHelper
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :set_locale
---
+++ app/helpers/svg_helper.rb
@@ -0,0 +1,7 @@
+module SvgHelper
+  def svg(name)
+    file_path = "#{Rails.root}/app/assets/images/icons/#{name}.svg"
+    return File.read(file_path).html_safe if File.exist?(file_path)
+    '(not found)'
+  end
+end
