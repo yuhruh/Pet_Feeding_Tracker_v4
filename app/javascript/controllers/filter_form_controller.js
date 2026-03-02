851d1126ec379030c4a96dbcb7c2665f6e2d498d
Rita <yuhruhgrape@gmail.com>
2026-03-02 22:58:15 +0800

    feat: extend submit time

--- app/javascript/controllers/filter_form_controller.js
+++ app/javascript/controllers/filter_form_controller.js
@@ -8,6 +8,6 @@ export default class extends Controller {
     clearTimeout(this.timeout)
     this.timeout = setTimeout(() => {
       this.element.requestSubmit()
-    }, 300)
+    }, 1000)
   }
 }
