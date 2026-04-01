32af370ff2844d29ca600cc2f1f3c361bdcd90ea
Rita <yuhruhgrape@gmail.com>
2026-04-01 11:22:55 +0800

    style: add reference range on liver check table

--- app/helpers/health_checks_helper.rb
+++ app/helpers/health_checks_helper.rb
@@ -1,2 +1,12 @@
 module HealthChecksHelper
+
+  def range_row_class(actual, high, low)
+    classes = [ "text-center" ]
+    if actual.to_f > high || actual.to_f < low
+      classes << "text-red-700 font-bold italic"
+    else 
+      classes << "font-normal"
+    end
+    classes.join(" ")
+  end
 end
