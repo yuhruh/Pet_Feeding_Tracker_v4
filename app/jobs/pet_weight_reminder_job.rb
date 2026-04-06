ed8bdb37a732dec378777fb3ee48562b5d7625f5
Rita <yuhruhgrape@gmail.com>
2026-04-06 23:09:31 +0800

    fix: fix pet weight nofication

--- app/jobs/pet_weight_reminder_job.rb
+++ app/jobs/pet_weight_reminder_job.rb
@@ -2,6 +2,16 @@ class PetWeightReminderJob < ApplicationJob
   queue_as :default
 
   def perform(user)
-    NotificationService.new(user).send_pet_weight_reminder
+    needs_reminder = user.pets.any? do |pet|
+      last_tracker = pet.trackers.where.not(weight: nil).order(date: :desc).first
+      last_weighed_date = last_tracker&.date || pet.created_at.to_date
+
+      days_since = (Date.current - last_weighed_date).to_i
+      days_since > 0 && days_since % 14 == 0
+    end
+
+    if needs_reminder
+      NotificationService.new(user).send_pet_weight_reminder
+    end
   end
 end
