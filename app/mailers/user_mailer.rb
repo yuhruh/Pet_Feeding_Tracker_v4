864169c5cc15eacbbb5f17ae88d9633b726ff442
Rita <yuhruhgrape@gmail.com>
2026-04-10 00:03:02 +0800

    style: fix nav bar style issue and i18n issue

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
-    mail(to: @user.email_address, cc: "ajicaretracker@gmail.com", subject: t(".subject"))
+    mail(to: @user.email_address, subject: t(".subject"))
   end
 
   def send_pet_weight_reminder(user)
     @user = user
-    mail(to: @user.email_address, cc: "ajicaretracker@gmail.com", subject: t(".subject"))
+    mail(to: @user.email_address, subject: t(".subject"))
   end
 end
