0056824de7a9256ca77e860472af30cda992633e
Rita <yuhruhgrape@gmail.com>
2026-04-11 15:43:41 +0800

    fix: fix rubocop offenses

--- app/jobs/user_backup_job.rb
+++ app/jobs/user_backup_job.rb
@@ -5,7 +5,7 @@ class UserBackupJob < ApplicationJob
     # Only send emails to users who have created at least one tracker in the last 5 days
     User.find_each do |user|
       has_recent_trackers = Tracker.joins(:pet).where(pets: { user_id: user.id }).where("trackers.created_at >= ?", 5.days.ago).exists?
-      
+
       if has_recent_trackers
         UserBackupMailer.backup_email(user).deliver_later
       end
