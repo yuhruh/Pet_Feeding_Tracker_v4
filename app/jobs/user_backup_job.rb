de723d5d207489fc058513b57cfa8e22cb0697ca
Rita <yuhruhgrape@gmail.com>
2026-04-11 15:36:37 +0800

    feat: skip backup email if no trackers created in 5 days

--- app/jobs/user_backup_job.rb
+++ app/jobs/user_backup_job.rb
@@ -2,8 +2,13 @@ class UserBackupJob < ApplicationJob
   queue_as :default
 
   def perform
+    # Only send emails to users who have created at least one tracker in the last 5 days
     User.find_each do |user|
-      UserBackupMailer.backup_email(user).deliver_later
+      has_recent_trackers = Tracker.joins(:pet).where(pets: { user_id: user.id }).where("trackers.created_at >= ?", 5.days.ago).exists?
+      
+      if has_recent_trackers
+        UserBackupMailer.backup_email(user).deliver_later
+      end
     end
   end
 end
