57b421842b8f83837aed8b710cfac7161b7489ef
Rita <yuhruhgrape@gmail.com>
2026-04-21 19:44:18 +0800

    feat: highlight first occurrence of food brand and description in trackers list

--- app/helpers/trackers_helper.rb
+++ app/helpers/trackers_helper.rb
@@ -11,6 +11,13 @@ module TrackersHelper
 
   def tracker_row_class(tracker)
     classes = []
+    
+    if first_occurrence?(tracker)
+      classes << "text-orange-600"
+    else
+      classes << "text-sky-700"
+    end
+
     if tracker.come_back_to_eat.blank?
       classes << "bg-yellow-100"
     elsif tracker.total_ate_amount.blank?
@@ -19,5 +26,22 @@ module TrackersHelper
       classes << "bg-blue-200"
     end
     classes.join(" ")
+  end
+
+  private
+
+  def first_occurrence?(tracker)
+    @first_occurrence_ids ||= {}
+    @first_occurrence_ids[tracker.pet_id] ||= begin
+      tracker.pet.trackers.select(:id, :brand, :description, :pet_id, :date, :feed_time).order(:date, :feed_time, :id).to_a.group_by do |t|
+        [t.brand.to_s.downcase.strip, normalize_description(t.description)]
+      end.map { |_, group| group.first.id }
+    end
+    @first_occurrence_ids[tracker.pet_id].include?(tracker.id)
+  end
+
+  def normalize_description(desc)
+    # Ignore x2, x3, etc. at the end of the description and normalize parentheses
+    desc.to_s.gsub("（", "(").gsub("）", ")").gsub(/\s*[xX]\d+\z/, "").squish.downcase
   end
 end
