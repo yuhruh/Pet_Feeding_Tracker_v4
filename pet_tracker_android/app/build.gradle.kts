d5dcd84f48e3dad4dcc9e6c5a3849d10bcd4c84b
Rita <yuhruhgrape@gmail.com>
2026-04-25 16:49:20 +0800

    Android: Bump versionCode to 5 and versionName to 1.0.4 with clean build

--- pet_tracker_android/app/build.gradle.kts
+++ pet_tracker_android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.pettracker.v4"
         minSdk = 28
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
