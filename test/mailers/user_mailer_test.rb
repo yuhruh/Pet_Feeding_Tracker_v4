f5f1f4dcad0935195cb5e6779aff1f2bb0263f7f
Rita <yuhruhgrape@gmail.com>
2026-05-01 21:04:05 +0800

    test: fix user mailer test expectations and body encoding matches

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "welcome_email" do
+    mail = UserMailer.with(user: users(:one)).welcome_email
+    assert_equal "Welcome to Cat Trackers App!", mail.subject
+    assert_equal [users(:one).email_address], mail.to
+    assert_equal ["ajicaretracker@gmail.com"], mail.from
+    assert_match /Welcome to Cat Tracker App/, mail.html_part.body.decoded
+  end
+
+  test "send_pet_weight_reminder" do
+    mail = UserMailer.send_pet_weight_reminder(users(:one))
+    assert_equal "Time to weigh your pet! 📝", mail.subject
+    assert_equal [users(:one).email_address], mail.to
+    assert_equal ["ajicaretracker@gmail.com"], mail.from
+    assert_match /Time to weigh your pet! 📝/, mail.body.decoded
+  end
 end
